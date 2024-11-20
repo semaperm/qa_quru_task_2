be0988a6108846999fabef766ecb2125372dd2e1
Sema Butsyk <semakontakt@mail.ru>
2024-11-20 22:20:04 +0500

    Фикс по замечанию ver.1

--- conftest.py
+++ conftest.py
@@ -4,10 +4,6 @@ from selene import browser
 @pytest.fixture(autouse=True)
 def browser_setting_1():
     browser.open("https://www.google.com/ncr")
-
-
-@pytest.fixture()
-def browser_setting_2(browser_setting_1):
     browser.config.window_height = 1080
     browser.config.window_width = 1920
 
--- test.py
+++ test.py
@@ -2,13 +2,11 @@ import pytest
 from selene import browser, be, have
 
 
-def test_one(browser_setting_2):
-    browser.open('https://google.com/ncr')
+def test_one():
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('[id="search"]').should(have.text('yashaka/selene: User-oriented Web UI browser tests in Python'))
 
 
-def test_two(browser_setting_2):
-    browser.open('https://google.com/ncr')
+def test_two():
     browser.element('[name="q"]').should(be.blank).type('12w3e=-!@#$%%^&$^&*$').press_enter()
     browser.element('#rcnt').should(have.text('Your search - 12w3e=-!@#$%%^&$^&*$ - did not match any documents.'))
